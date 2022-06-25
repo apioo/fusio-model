e387469ffc270b1f9e20a9683d92e20dad60d9d1
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-06-25 17:57:23 +0200

    add sdk response

---
+++ src/Backend/Sdk_Response.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Fusio\Model\Backend;
+
+
+class Sdk_Response implements \JsonSerializable
+{
+    protected ?Sdk_Types $types = null;
+    public function setTypes(?Sdk_Types $types) : void
+    {
+        $this->types = $types;
+    }
+    public function getTypes() : ?Sdk_Types
+    {
+        return $this->types;
+    }
+    public function jsonSerialize() : \stdClass
+    {
+        return (object) array_filter(array('types' => $this->types), static function ($value) : bool {
+            return $value !== null;
+        });
+    }
+}
+
